eafcc3c951e701adbdf1d5672d64d59988e12a03
TheoHauser <theo.hauser@temple.edu>
2016-01-13 10:23:12 -0500

    Maze Project just getting started

---
+++ src/maze3d/MazeCell.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maze3d;
+
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author Theo
+ */
+public class MazeCell extends Node{
+    public static Box wall = new Box();
+    
+}
